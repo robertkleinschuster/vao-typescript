b6e6f759eabc022eceb1d9af8ebb67af64816340
robertkleinschuster <robertkleinschuster@users.noreply.github.com>
2023-12-11 10:40:32 +0000

    Generate client

---
+++ typescript-fetch-client/apis/index.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AddresslookupApi';
+export * from './ArrivalBoardApi';
+export * from './DatainfoApi';
+export * from './DefaultApi';
+export * from './DepartureBoardApi';
+export * from './GisrouteApi';
+export * from './HimsearchApi';
+export * from './JourneyDetailApi';
+export * from './LocationDetailsApi';
+export * from './LocationNameApi';
+export * from './LocationNearbystopsApi';
+export * from './ReconApi';
+export * from './TripApi';
+export * from './TtiApi';
